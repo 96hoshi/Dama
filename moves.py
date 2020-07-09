eebda38f03f51880b27e7ccdd02840d1f997903c
96hoshi <m.locascio96@hotmail.com>
2020-07-09 12:38:25 +0200

    Fixed paths evaluation, now module saves list of paths

--- moves.py
+++ moves.py
@@ -21,7 +21,7 @@ DAMAB = "B-D"
 # assicurarsi che color sia binario
 
 max_eat = 0
-max_path = []
+max_paths = []
 max_eaten_order = []
 max_is_dama = 0
 
@@ -56,59 +56,70 @@ def is_dama(string):
 
 def clear_max():
     global max_eat
-    global max_path
+    global max_paths
     global max_eaten_order
     global max_is_dama
 
     max_eat = 0
-    max_path = []
+    max_paths = []
     max_eaten_order = []
     max_is_dama = 0
 
 def eval_and_register_path(path, eaten, dama):
     global max_eat
-    global max_path
+    global max_paths
     global max_eaten_order
     global max_is_dama
 
     n_eat = len(eaten)
     print(n_eat, path, eaten, dama)
 
-    if n_eat < max_eat:
-        print("not the best")
-        return
+    if n_eat > max_eat:
+        max_eat = n_eat
+        max_paths.clear()
+        max_paths.append(path)
+        max_eaten_order = eaten
+        max_is_dama = dama
 
-    if n_eat == max_eat:
-        if max_is_dama and not dama:
-            print("dama has priority")
-            return
-        # entrambe sono dama
-        if max_is_dama and dama:
+    elif n_eat == max_eat:
+        if (not max_is_dama) and (not dama):
+            max_paths.append(path)
+
+        elif (not max_is_dama) and dama:
+            max_eat = n_eat
+            max_paths.clear()
+            max_paths.append(path)
+            max_eaten_order = eaten
+            max_is_dama = dama
+
+            # entrambe sono dama
+        elif max_is_dama and dama:
             # confronta chi ha mangiato più dame
             d_eaten = eaten.count("d")
-            # print("d_eaten: ", d_eaten)
             d_max_eaten = max_eaten_order.count("d")
-            if d_eaten < d_max_eaten:
-                return
+
+            if d_eaten > d_max_eaten:
+                max_eat = n_eat
+                max_paths.clear()
+                max_paths.append(path)
+                max_eaten_order = eaten
+                max_is_dama = dama
 
             # se hanno mangiato lo stesso numero di dame
-            if d_eaten == d_max_eaten and d_eaten > 0:
-                # trovo il path che ha incontrato prima una dama
-                if eaten.index("d") > max_eaten_order.index("d"):
-                    return
-                if eaten.index("d") == max_eaten_order.index("d"):
-                    # caso particolare: entrambe le scelte sono equivalenti
-                    # devo aggiungere entrambe alle mosse obbligate
-                    # da modificarein lista di liste
-                    return
-# update values
-    max_eat = n_eat
-    max_path = []
-    # max_path.append(path)
-    max_path = path
-    max_eaten_order = eaten
-    max_is_dama = dama
-    print("-----------", path, max_path)
+            elif d_eaten == d_max_eaten:
+                if d_eaten > 0:
+                    # trovo il path che ha incontrato prima una dama
+                    if eaten.index("d") < max_eaten_order.index("d"):
+                        max_eat = n_eat
+                        max_paths.clear()
+                        max_paths.append(path)
+                        max_eaten_order = eaten
+                        max_is_dama = dama
+
+                    elif eaten.index("d") == max_eaten_order.index("d"):
+                        max_paths.append(path)
+                else:
+                    max_paths.append(path)
 
 def copy_new_board(i, j, end_i, end_j, eat, board):
     new_board = deepcopy(board)
@@ -128,11 +139,12 @@ def board_forced_moves(color, board):
             if color_check(i, j, color, board):
                 calculate_forced_moves(i, j, color, [(i, j)], [], is_dama(board[i][j]), board)
 
-    print(max_path)
-    return max_path
+    print("Forced paths:", max_paths)
+    return max_paths
 
 
 def calculate_forced_moves(i, j, color, path, eaten, dama, board):
+    stop = True
     dir = 1
     if color == WHITE:
         dir = -1
@@ -149,29 +161,30 @@ def calculate_forced_moves(i, j, color, path, eaten, dama, board):
                     #copia della nuova board con la mossa effettuata
                     new_board = copy_new_board(i, j, i + dir*2, j - 2, True, board)
                     calculate_forced_moves(i + dir*2, j - 2, color, path, eaten, dama, new_board)
+                    stop = False
                 elif dama:
                     eaten.append("d") #ho mangiato una dama
                     path.append((i + dir*2, j - 2))
-                    #copia della nuova board con la mossa effettuata
                     new_board = copy_new_board(i, j, i + dir*2, j - 2, True, board)
                     calculate_forced_moves(i + dir*2, j - 2, color, path, eaten, dama, new_board)
+                    stop = False
 
     # sinistra
     if box_legal(i + dir, j + 1) and board[i + dir][j + 1] != EMPTY:
         if not color_check(i + dir, j + 1, color, board):
             if box_legal(i + dir*2, j + 2) and board[i + dir*2][j + 2] == EMPTY:
                 if not is_dama(board[i + dir][j + 1]):
-                    eaten.append("p") #ho mangiato una pedina
+                    eaten.append("p")
                     path.append((i + dir*2, j + 2))
-                    #copia della nuova board con la mossa effettuata
                     new_board = copy_new_board(i, j, i + dir*2, j + 2, True, board)
                     calculate_forced_moves(i + dir*2, j + 2, color, path, eaten, dama, new_board)
+                    stop = False
                 elif dama:
-                    eaten.append("d") #ho mangiato una dama
+                    eaten.append("d")
                     path.append((i + dir*2, j + 2))
-                    #copia della nuova board con la mossa effettuata
                     new_board = copy_new_board(i, j, i + dir*2, j + 2, True, board)
                     calculate_forced_moves(i + dir*2, j + 2, color, path, eaten, dama, new_board)
+                    stop = False
 
     # se sono una dama valuto anche e altre due direzioni
     if dama:
@@ -183,38 +196,38 @@ def calculate_forced_moves(i, j, color, path, eaten, dama, board):
                 if box_legal(i + dir*2, j - 2) and board[i + dir*2][j - 2] == EMPTY:
                     # e la casella dopo è libera, posso mangiare
                     if not is_dama(board[i + dir][j - 1]):
-                        eaten.append("p") #ho mangiato una pedina
+                        eaten.append("p")
                         path.append((i + dir*2, j - 2))
-                        #copia della nuova board con la mossa effettuata
                         new_board = copy_new_board(i, j, i + dir*2, j - 2, True, board)
                         calculate_forced_moves(i + dir*2, j - 2, color, path, eaten, dama, new_board)
+                        stop = False
                     else:
-                        eaten.append("d") #ho mangiato una dama
+                        eaten.append("d")
                         path.append((i + dir*2, j - 2))
-                        #copia della nuova board con la mossa effettuata
                         new_board = copy_new_board(i, j, i + dir*2, j - 2, True, board)
                         calculate_forced_moves(i + dir*2, j - 2, color, path, eaten, dama, new_board)
+                        stop = False
 
         # sinistra
         if box_legal(i + dir, j + 1) and board[i + dir][j + 1] != EMPTY:
             if not color_check(i + dir, j + 1, color, board):
                 if box_legal(i + dir*2, j + 2) and board[i + dir*2][j + 2] == EMPTY:
                     if not is_dama(board[i + dir][j + 1]):
-                        eaten.append("p") #ho mangiato una pedina
+                        eaten.append("p")
                         path.append((i + dir*2, j + 2))
-                        #copia della nuova board con la mossa effettuata
                         new_board = copy_new_board(i, j, i + dir*2, j + 2, True, board)
                         calculate_forced_moves(i + dir*2, j + 2, color, path, eaten, dama, new_board)
+                        stop = False
 
                     else:
-                        eaten.append("d") #ho mangiato una dama
+                        eaten.append("d")
                         path.append((i + dir*2, j + 2))
-                        #copia della nuova board con la mossa effettuata
                         new_board = copy_new_board(i, j, i + dir*2, j + 2, True, board)
                         calculate_forced_moves(i + dir*2, j + 2, color, path, eaten, dama, new_board)
+                        stop = False
 
 
     # se arrivo fino in fondo e nel path c'è più di una mossa
-    if len(path) > 1:
+    if stop and len(path) > 1:
         eval_and_register_path(path, eaten, dama)
         # aggiungi la mossa al set di mosse (poi verrà confrontata)
