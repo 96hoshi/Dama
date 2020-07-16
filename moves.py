2c87185fa4a0fceb113a479a8879211299fb3b4d
96hoshi <m.locascio96@hotmail.com>
2020-07-16 19:36:23 +0200

    Now contains dama rules engine

--- moves.py
+++ moves.py
@@ -12,24 +12,26 @@ BLACK = "B"
 DAMAW = "W-D"
 DAMAB = "B-D"
 
-# PEDINE:
-# White
-# Black
-# White-Dama
-# Black-Dama
-# TODO:
-# assicurarsi che color sia binario
-
 max_eat = 0
 max_paths = []
 max_eaten_order = []
 max_is_dama = 0
 
-# color:
-# white = WHITE or DAMAW
-# black = BLACK or DAMAB
-
 # ----------------------------------------------------
+
+# print a dama board
+def print_board(board):
+    print('    ', end = "")
+    for c in range(SIZE):
+        print('{:^4}'.format(c), end = "")
+    print()
+    print("   ---------------------------------")
+    for r in range(SIZE):
+        print("{:^3}".format(r), end = "")
+        for c in range(SIZE):
+            print('|{:^3}'.format(board[r][c]), end = "")
+        print("|")
+        print("   ---------------------------------")
 
 # check if the pedina is inside the board
 def box_legal(i, j):
@@ -49,10 +51,223 @@ def eat_pedina(i, j, end_i, end_j, board):
 
     board[delete_i][delete_j] = EMPTY
 
-# ----------------------------------------------------
+def ask_input(string):
+    while True:
+        try:
+            str_i, str_j = input(string).split()
+        except ValueError:
+            print("Wrong input, correct usage: <int, int>")
+            continue
+        else:
+            return str_i, str_j
+
+def is_int(string):
+    try: 
+        int(string)
+        return True
+    except ValueError:
+        return False
+
+def ask_box(string):
+    str_i, str_j = ask_input(string)
+    while (not is_int(str_i)) and (not is_int(str_j)):
+        print("Wrong input, correct usage: <int, int>")
+        continue
+
+    i = int(str_i)
+    j = int(str_j)
+    return i, j
 
 def is_dama(string):
     return string == DAMAB or string == DAMAW
+
+# check if the pedina becomes dama
+def check_new_dama(i, j, board):
+    if board[i][j] == WHITE  and i == 0:
+        board[i][j] = DAMAW
+        return True
+
+    if board[i][j] == BLACK and i == 7:
+        board[i][j] = DAMAB
+        return True
+
+    return False
+
+# check if there's only one color in the board
+def win_condition(board):
+    color = 0
+
+    for i in range(SIZE):
+        for j in range(SIZE):
+            if board[i][j] == EMPTY:
+                continue
+            if board[i][j] == WHITE or board[i][j] == DAMAW:
+                if color == 0 or color == 1:
+                    color = 1
+                    continue
+                else:
+                    return False
+            if board[i][j] == BLACK or board[i][j] == DAMAB:
+                if color == 0 or color == 2:
+                    color = 2
+                    continue
+                else:
+                    return False
+
+    return True
+
+# returns list of legal moves from a position
+# if there's no pedina in there returns empty list
+# ASSUMPTION: (i, j) is a board position
+def legal_moves(i, j, board):
+    legal_m = []
+
+    is_dama = board[i][j] == DAMAW or board[i][j] == DAMAB
+
+    if board[i][j] == WHITE or is_dama:
+        if box_legal(i - 1, j - 1) and board[i - 1][j - 1] == EMPTY:
+            legal_m.append((i - 1, j - 1))
+        if box_legal(i - 1, j + 1) and board[i - 1][j + 1] == EMPTY:
+            legal_m.append((i - 1, j + 1))
+
+    if board[i][j] == BLACK or is_dama:
+        if box_legal(i + 1, j - 1) and board[i + 1][j - 1] == EMPTY:
+            legal_m.append((i + 1, j - 1))
+        if box_legal(i + 1, j + 1) and board[i + 1][j + 1] == EMPTY:
+            legal_m.append((i + 1, j + 1))
+
+    return legal_m
+
+def check_if_empty(list_of_lists):
+    for elem in list_of_lists:
+        if elem:
+            return False
+    return True
+
+def update_forced_moves(i, j, all_forced_m):
+    updated_forced_m = []
+    for path in all_forced_m:
+        if path[0] == (i, j):
+            path.pop(0)
+            updated_forced_m.append(path)
+
+    return updated_forced_m
+
+def search_forced_move(i, j, all_forced_m):
+    for path in all_forced_m:
+        if path[0] == (i, j):
+            return True
+
+    return False
+
+# changes the board moving a pedina.
+# if and only if (end_i, end_j) are legal_moves
+# if there's a forced move to do and it's not (end_i, end_j) it is an illegal move
+def try_move(i, j, end_i, end_j, all_forced_m, board):
+    l_moves = legal_moves(i, j, board)
+
+    # there's a forced move but it's not the selected one
+    if not check_if_empty(all_forced_m):
+        if search_forced_move(end_i, end_j, all_forced_m):
+            all_forced_m = update_forced_moves(end_i, end_j, all_forced_m)
+            eat_pedina(i, j, end_i, end_j, board)
+        else:
+            print("There are forced moves to do: ", all_forced_m)
+            return False
+
+    # there's a legal move but it's not the selected one
+    elif l_moves:
+        if (end_i, end_j) not in l_moves:
+            print("Not a legal move")
+            return False
+        else:
+            l_moves.clear()
+
+    board[end_i][end_j] = board[i][j]
+    board[i][j] = EMPTY
+
+    check_new_dama(end_i, end_j, board)
+
+    return True
+
+# ORDINAMENTO:
+# Avendo più possibilità di presa si debbono rispettare obbligatoriamente
+# nell'ordine le seguenti priorità:
+#DONE # è obbligatorio mangiare dove ci sono più pezzi;
+#DONE # a parità di pezzi da prendere, tra pedina e dama si è obbligati a mangiare la dama;
+#       inoltre se si può optare tra il mangiare di dama o di pedina
+#       è obbligatorio mangiare con la dama;
+#DONE # la dama sceglie la presa dove si mangiano più dame;
+#DONE # a parità  di condizioni si mangia dove s'incontra prima la dama avversaria.
+
+# TODO:
+# Si vince per abbandono dell'avversario, che si trova in palese difficoltà,
+# o quando si catturano o si bloccano tutti i pezzi avversari.
+
+# TODO:
+# Si pareggia in una situazione di evidente equilibrio finale per accordo
+# dei giocatori o per decisione dell'arbitro a seguito del conteggio di 40 mosse
+# richiesto da uno dei due giocatori.
+# Il conteggio delle mosse si azzera e riparte da capo tutte le volte che
+# uno dei due giocatori muove una pedina o effettua una presa.
+
+def human_turn(player_color, board):
+    all_forced_m = board_forced_moves(player_color, board)
+    print("You are player: " + player_color)
+
+    # first position loop
+    while True:
+        # ask for pedina to move
+        i, j = ask_box("Enter box location: ")
+
+        # check if there is a pedina of player color
+        if not (box_legal(i, j) and color_check(i, j, player_color, board)):
+            print("Not a legal position")
+            continue
+
+        # check if there are moves to do first
+        if not check_if_empty(all_forced_m):
+            if search_forced_move(i, j, all_forced_m):
+                all_forced_m = update_forced_moves(i, j, all_forced_m)
+            else:
+                print("There are forced moves to do first: ", all_forced_m)
+                continue
+        else:
+            # if there are no possible actions from that position
+            legal_m = legal_moves(i, j, board)
+            if not legal_m:
+                print("No possible actions from", i, j)
+                continue
+        break
+
+    # destination move loop
+    while True:
+        # ask for destination box
+        end_i, end_j = ask_box("Enter destination box: ")
+
+        # check if is legal board position
+        if not box_legal(end_i, end_j):
+            print("Position out of the board")
+            continue
+
+        # try to perform the move
+        if not try_move(i, j, end_i, end_j, all_forced_m, board):
+            continue
+
+        print_board(board)
+
+        # if there are still move to do
+        if not check_if_empty(all_forced_m):
+            print("There are still forced moves to do:", all_forced_m)
+            i = end_i
+            j = end_j
+            continue
+        # end destination move loop
+        break
+
+    return win_condition(board)
+
+# ----------------------------------------------------
 
 def clear_max():
     global max_eat
@@ -65,6 +280,18 @@ def clear_max():
     max_eaten_order = []
     max_is_dama = 0
 
+def update_max(n_eat, path, eaten, dama):
+    global max_eat
+    global max_paths
+    global max_eaten_order
+    global max_is_dama
+
+    max_eat = n_eat
+    max_paths.clear()
+    max_paths.append(path)
+    max_eaten_order = eaten
+    max_is_dama = dama
+
 def eval_and_register_path(path, eaten, dama):
     global max_eat
     global max_paths
@@ -72,51 +299,34 @@ def eval_and_register_path(path, eaten, dama):
     global max_is_dama
 
     n_eat = len(eaten)
-    print(n_eat, path, eaten, dama)
+    # print(n_eat, path, eaten, dama)
 
     if n_eat > max_eat:
-        max_eat = n_eat
-        max_paths.clear()
-        max_paths.append(path)
-        max_eaten_order = eaten
-        max_is_dama = dama
-
+        update_max(n_eat, path, eaten, dama)
     elif n_eat == max_eat:
+
         if (not max_is_dama) and (not dama):
             max_paths.append(path)
-
         elif (not max_is_dama) and dama:
-            max_eat = n_eat
-            max_paths.clear()
-            max_paths.append(path)
-            max_eaten_order = eaten
-            max_is_dama = dama
-
+            update_max(n_eat, path, eaten, dama)
             # entrambe sono dama
         elif max_is_dama and dama:
+
             # confronta chi ha mangiato più dame
             d_eaten = eaten.count("d")
             d_max_eaten = max_eaten_order.count("d")
-
             if d_eaten > d_max_eaten:
-                max_eat = n_eat
-                max_paths.clear()
-                max_paths.append(path)
-                max_eaten_order = eaten
-                max_is_dama = dama
-
+                update_max(n_eat, path, eaten, dama)
             # se hanno mangiato lo stesso numero di dame
             elif d_eaten == d_max_eaten:
                 if d_eaten > 0:
-                    # trovo il path che ha incontrato prima una dama
-                    if eaten.index("d") < max_eaten_order.index("d"):
-                        max_eat = n_eat
-                        max_paths.clear()
-                        max_paths.append(path)
-                        max_eaten_order = eaten
-                        max_is_dama = dama
 
-                    elif eaten.index("d") == max_eaten_order.index("d"):
+                    # trovo il path che ha incontrato prima una dama
+                    index_eaten = eaten.index("d")
+                    index_max_eaten = max_eaten_order.index("d")
+                    if index_eaten < index_max_eaten:
+                        update_max(n_eat, path, eaten, dama)
+                    elif index_eaten == index_max_eaten:
                         max_paths.append(path)
                 else:
                     max_paths.append(path)
@@ -139,7 +349,7 @@ def board_forced_moves(color, board):
             if color_check(i, j, color, board):
                 calculate_forced_moves(i, j, color, [(i, j)], [], is_dama(board[i][j]), board)
 
-    print("Forced paths:", max_paths)
+    # print("Forced paths:", max_paths)
     return max_paths
 
 def check_direction(i, dir_i, j, dir_j, color, path, eaten, dama, board):
