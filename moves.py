4ad04eb475ce9bdbd3ea2a136f5ce40ef152dbc9
96hoshi <m.locascio96@hotmail.com>
2020-09-25 19:58:15 +0200

    Added utility funcions, now no flag is needed to perform a eating move

--- moves.py
+++ moves.py
@@ -7,10 +7,10 @@ from copy import copy, deepcopy
 SIZE = 8
 
 EMPTY = " "
-WHITE = ' \033[91m' + "W" + '\033[0m '#"W"
-BLACK = ' \033[94m' + "B" + '\033[0m '#"B"
-DAMAW = '\033[91m' + "W-D" + '\033[0m'#"W-D"
-DAMAB = '\033[94m' + "B-D" + '\033[0m'#"B-D"
+WHITE = ' \033[91m' + "W" + '\033[0m '
+BLACK = ' \033[94m' + "B" + '\033[0m '
+DAMAW = '\033[91m' + "W-D" + '\033[0m'
+DAMAB = '\033[94m' + "B-D" + '\033[0m'
 
 max_eat = 0
 max_paths = []
@@ -33,23 +33,13 @@ def print_board(board):
         print("|")
         print("   ---------------------------------")
 
-def print_position(pos, color):
+def print_position(position, color):
     c = '\033[94m'
     if color == WHITE:
         c = '\033[91m'
 
-    i, j = pos
+    i, j = position
     print(" {}{},{}\033[0m".format(c, i, j), end = "")
-
-def print_move(move, color):
-    first = True
-
-    for m in move:
-        print_position(m[0], color)
-        print(" ->", end = "")
-        print_position(m[1], color)
-        print(", ", end = "")
-    print()
 
 def print_movement(m, color):
     if not m:
@@ -59,6 +49,17 @@ def print_movement(m, color):
     print(" ->", end = "")
     print_position(m[1], color)
     print()
+
+# DEBUG print
+# def print_move(move, color):
+#     for pos in move:
+#         print_position(pos[0], color)
+#         print(" ->", end = "")
+#         print_position(pos[1], color)
+#         print(", ", end = "")
+#     print()
+
+# ----------------------------------------------------
 
 # check if the pedina is inside the board
 def box_legal(i, j):
@@ -71,12 +72,33 @@ def color_check(i, j, color, board):
     else:
         return board[i][j] == BLACK or board[i][j] == DAMAB
 
+def opposite_color(color):
+    if color == WHITE:
+        return BLACK
+    else:
+        return WHITE
+
 # removes the eaten pedina between two positions
 def eat_pedina(i, j, end_i, end_j, board):
     delete_i = (i + end_i) // 2
     delete_j = (j + end_j) // 2
 
     board[delete_i][delete_j] = EMPTY
+
+def is_dama(string):
+    return string == DAMAB or string == DAMAW
+
+# check if the pedina becomes dama
+def check_new_dama(i, j, board):
+    if board[i][j] == WHITE  and i == 0:
+        board[i][j] = DAMAW
+        return True
+
+    if board[i][j] == BLACK and i == 7:
+        board[i][j] = DAMAB
+        return True
+
+    return False
 
 def ask_input(string):
     while True:
@@ -105,41 +127,27 @@ def ask_box(string):
     j = int(str_j)
     return i, j
 
-def is_dama(string):
-    return string == DAMAB or string == DAMAW
+def win_condition(color, board):
+#   checks if the enemy have no moves to perform
+    enemy_color = opposite_color(color)
 
-# check if the pedina becomes dama
-def check_new_dama(i, j, board):
-    if board[i][j] == WHITE  and i == 0:
-        board[i][j] = DAMAW
-        return True
+    enemy_legal = board_legal_moves(enemy_color, board)
+    if not check_if_empty(enemy_legal):
+        return False
 
-    if board[i][j] == BLACK and i == 7:
-        board[i][j] = DAMAB
-        return True
+    enemy_forced = board_forced_moves(enemy_color, board)
+    if not check_if_empty(enemy_forced):
+        return False
 
-    return False
-
-# check if there's only one color in the board
-def win_condition(board):
-    color = 0
-
+#   check if there is only player color on the board
     for i in range(SIZE):
         for j in range(SIZE):
             if board[i][j] == EMPTY:
                 continue
-            if board[i][j] == WHITE or board[i][j] == DAMAW:
-                if color == 0 or color == 1:
-                    color = 1
-                    continue
-                else:
-                    return False
-            if board[i][j] == BLACK or board[i][j] == DAMAB:
-                if color == 0 or color == 2:
-                    color = 2
-                    continue
-                else:
-                    return False
+            if color_check(i, j, color, board):
+                continue
+            if color_check(i, j, enemy_color, board):
+                return False
 
     return True
 
@@ -149,15 +157,15 @@ def win_condition(board):
 def legal_moves(i, j, board):
     legal_m = []
 
-    is_dama = board[i][j] == DAMAW or board[i][j] == DAMAB
+    dama = is_dama(board[i][j])
 
-    if board[i][j] == WHITE or is_dama:
+    if color_check(i, j, WHITE, board) or dama:
         if box_legal(i - 1, j - 1) and board[i - 1][j - 1] == EMPTY:
             legal_m.append((i - 1, j - 1))
         if box_legal(i - 1, j + 1) and board[i - 1][j + 1] == EMPTY:
             legal_m.append((i - 1, j + 1))
 
-    if board[i][j] == BLACK or is_dama:
+    if color_check(i, j, BLACK, board) or dama:
         if box_legal(i + 1, j - 1) and board[i + 1][j - 1] == EMPTY:
             legal_m.append((i + 1, j - 1))
         if box_legal(i + 1, j + 1) and board[i + 1][j + 1] == EMPTY:
@@ -165,6 +173,22 @@ def legal_moves(i, j, board):
 
     return legal_m
 
+# returna a list of possible legal moves on the board
+# for the player of that color
+# returns a list of lists
+def board_legal_moves(color, board):
+    all_legal_m = []
+
+    for i in range(SIZE):
+        for j in range(SIZE):
+            if color_check(i, j, color, board):
+                l_m = legal_moves(i, j, board)
+                for move in l_m:
+                    all_legal_m.append([(i, j), move])
+
+    return all_legal_m
+
+# check if a list of lists is empty
 def check_if_empty(list_of_lists):
     for elem in list_of_lists:
         if elem:
@@ -187,23 +211,30 @@ def search_forced_move(i, j, all_forced_m):
 
     return False
 
-def movement(i, j, end_i, end_j, eat, board):
+# perform a single movement froma (i,j) to (end_i,end_j)
+# removing enemy pedina if needed
+def movement(i, j, end_i, end_j, board):
     board[end_i][end_j] = board[i][j]
     board[i][j] = EMPTY
-    if eat:
-        eat_pedina(i, j, end_i, end_j, board)
 
-# changes the board moving a pedina.
+    diff_i = abs(i - end_i)
+    diff_j = abs(j - end_j)
+
+    if diff_i == 2 or diff_j == 2:
+        eat_pedina(i, j, end_i, end_j, board)
+        return True
+    return False
+
+
+# check if is possible to updates the board moving a pedina.
 # if and only if (end_i, end_j) are legal_moves
 # if there's a forced move to do and it's not (end_i, end_j) it is an illegal move
 def try_move(i, j, end_i, end_j, all_forced_m, board):
-    eat = False
 
     # there's a forced move but it's not the selected one
     if not check_if_empty(all_forced_m):
         if search_forced_move(end_i, end_j, all_forced_m):
             all_forced_m = update_forced_moves(end_i, end_j, all_forced_m)
-            eat = True
         else:
             print("There are forced moves to do: ", all_forced_m)
             return False
@@ -217,7 +248,7 @@ def try_move(i, j, end_i, end_j, all_forced_m, board):
             else:
                 l_moves.clear()
 
-    movement(i, j, end_i, end_j, eat, board)
+    movement(i, j, end_i, end_j, board)
     check_new_dama(end_i, end_j, board)
 
     return True
@@ -235,6 +266,7 @@ def try_move(i, j, end_i, end_j, all_forced_m, board):
 # TODO:
 # Si vince per abbandono dell'avversario, che si trova in palese difficoltà,
 # o quando si catturano o si bloccano tutti i pezzi avversari.
+# DONE
 
 # TODO:
 # Si pareggia in una situazione di evidente equilibrio finale per accordo
@@ -244,10 +276,17 @@ def try_move(i, j, end_i, end_j, all_forced_m, board):
 # uno dei due giocatori muove una pedina o effettua una presa.
 
 def human_turn(player_color, board):
-    all_forced_m = board_forced_moves(player_color, board)
-    print("You are player: " + player_color)
+    print("{} turn:".format(player_color))
+    eat_move = False
 
-    # first position loop
+    all_forced_m = board_forced_moves(player_color, board)
+    all_legal_m = board_legal_moves(player_color, board)
+
+    if (not all_forced_m) and (not all_legal_m):
+        print("No possible moves to do")
+        return False, opposite_color(player_color), True
+
+    # first box position loop
     while True:
         # ask for pedina to move
         i, j = ask_box("Enter box location: ")
@@ -259,6 +298,7 @@ def human_turn(player_color, board):
 
         # check if there are moves to do first
         if not check_if_empty(all_forced_m):
+            eat_move = True
             if search_forced_move(i, j, all_forced_m):
                 all_forced_m = update_forced_moves(i, j, all_forced_m)
             else:
@@ -272,7 +312,7 @@ def human_turn(player_color, board):
                 continue
         break
 
-    # destination move loop
+    # destination box for movement loop
     while True:
         # ask for destination box
         end_i, end_j = ask_box("Enter destination box: ")
@@ -297,7 +337,7 @@ def human_turn(player_color, board):
         # end destination move loop
         break
 
-    return win_condition(board)
+    return eat_move, player_color, win_condition(player_color, board)
 
 # ----------------------------------------------------
 
@@ -325,10 +365,7 @@ def update_max(n_eat, path, eaten, dama):
     max_is_dama = dama
 
 def register_path(path, eaten, dama):
-    global max_eat
     global max_paths
-    global max_eaten_order
-    global max_is_dama
 
     n_eat = len(eaten)
 
@@ -374,12 +411,13 @@ def copy_new_path(path, elem):
 
     return new_path
 
-def copy_new_board(i, j, end_i, end_j, eat, board):
+def copy_new_board(i, j, end_i, end_j, board):
     new_board = deepcopy(board)
-    movement(i, j, end_i, end_j, eat, new_board)
+    movement(i, j, end_i, end_j, new_board)
 
     return new_board
 
+# returns a list of lists
 def board_forced_moves(color, board):
     clear_max()
 
@@ -392,21 +430,23 @@ def board_forced_moves(color, board):
 
 def check_direction(i, dir_i, j, dir_j, color, path, eaten, dama, board):
     if box_legal(i + dir_i, j + dir_j) and board[i + dir_i][j + dir_j] != EMPTY:
+        # se la casella è occupata da una pedina avversaria
         if not color_check(i + dir_i, j + dir_j, color, board):
-            # se la casella dx è occupata da una pedina avversaria
+            # e la casella dopo è libera
             if box_legal(i + dir_i*2, j + dir_j*2) and board[i + dir_i*2][j + dir_j*2] == EMPTY:
-                # e la casella dopo è libera, posso mangiare
+                # non è una dama
                 if not is_dama(board[i + dir_i][j + dir_j]):
                     new_eaten = copy_new_eaten(eaten,"p") #ho mangiato una pedina semplice
                     new_path = copy_new_path(path, (i + dir_i*2, j + dir_j*2))
                     # copia della nuova board con la mossa effettuata
-                    new_board = copy_new_board(i, j, i + dir_i*2, j + dir_j*2, True, board)
+                    new_board = copy_new_board(i, j, i + dir_i*2, j + dir_j*2, board)
                     calculate_forced_moves(i + dir_i*2, j + dir_j*2, color, new_path, new_eaten, dama, new_board)
                     return False
+                # è una dama e anche io sono una dama
                 elif dama:
                     new_eaten = copy_new_eaten(eaten,"d") #ho mangiato una dama
                     new_path = copy_new_path(path, (i + dir_i*2, j + dir_j*2))
-                    new_board = copy_new_board(i, j, i + dir_i*2, j + dir_j*2, True, board)
+                    new_board = copy_new_board(i, j, i + dir_i*2, j + dir_j*2, board)
                     calculate_forced_moves(i + dir_i*2, j + dir_j*2, color, new_path, new_eaten, dama, new_board)
                     return False
 
@@ -417,14 +457,13 @@ def calculate_forced_moves(i, j, color, path, eaten, dama, board):
     dir_i = 1
     if color == WHITE:
         dir_i = -1
-    # dir_j = -1 se dx
+
+    # se dx allora dir_j = -1
     dx = -1
-    # dir_j = 1 se sx
+    # se sx allora dir_j = 1
     sx = 1
 
-    # destra
     stop = check_direction(i, dir_i, j, dx, color, path, eaten, dama, board)
-    # sinistra
     stop = check_direction(i, dir_i, j, sx, color, path, eaten, dama, board)
 
     # se sono una dama valuto anche le altre due direzioni
@@ -433,6 +472,6 @@ def calculate_forced_moves(i, j, color, path, eaten, dama, board):
         stop = check_direction(i, dir_i, j, dx, color, path, eaten, dama, board)
         stop = check_direction(i, dir_i, j, sx, color, path, eaten, dama, board)
 
+    # aggiungi la mossa al set di mosse (poi verrà confrontata)
     if stop and len(path) > 1:
         register_path(path, eaten, dama)
-        # aggiungi la mossa al set di mosse (poi verrà confrontata)
