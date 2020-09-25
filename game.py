e2e1011851d650e06be561bb3e14813408c9853a
96hoshi <m.locascio96@hotmail.com>
2020-09-25 19:52:27 +0200

    Now player can choose a color and implemented draw counter

--- game.py
+++ game.py
@@ -2,10 +2,12 @@
 
 import random
 import time
+import functools
 from moves import *
 from ia_dama import ia_turn
-import functools
 
+draw_white = 0
+draw_black = 0
 
 def timer(func):
     """Print the runtime of the decorated function"""
@@ -34,7 +36,25 @@ def start_board(board):
             elif c % 2 != 0 and r % 2 != 0:
                     board[r][c] = WHITE
 
-def end_game(end, draw, color):
+def increment_draw(color):
+    global draw_white, draw_black
+
+    if color == WHITE:
+        draw_white += 1
+        return draw_white
+    else:
+        draw_black += 1
+        return draw_black
+
+def reset_draw(color):
+    global draw_white, draw_black
+
+    if color == WHITE:
+        draw_white = 0
+    else:
+        draw_black = 0
+
+def game_over(eat_move, color, end):
     winner = "Whites"
     if color == BLACK:
         winner = "Blacks"
@@ -42,11 +62,35 @@ def end_game(end, draw, color):
     if end:
         print(winner + " win!")
         return True
-    if draw == 40:
-        print("Draw!")
-        return True
+    if eat_move:
+        reset_draw(color)
+    else:
+        draw_counter = increment_draw(color)
+        if draw_counter == 40:
+            print("Draw!")
+            return True
 
     return False
+
+def ask_color():
+    while True:
+        try:
+            c = input("Choose your color:\033[91m w\033[0m / \033[94m b \033[0m\n").lower()
+        except ValueError:
+            print("Wrong input, correct usage: <string>")
+            continue
+        else:
+            if c == "w" or c == "b":
+                return c
+            print("Wrong input, correct usage: w/b")
+            continue
+
+def is_player_first(c):
+    if c == "w":
+        # player choose white, first move to perform
+        return True
+    else:
+        return False
 
 @timer
 def main():
@@ -56,27 +100,40 @@ def main():
     start_board(board)
     print_board(board)
 
-    # game loop
-    while True:
-        # end = human_turn(WHITE, board)
-        # if end:
-        #     print("Whites win!")
-        #     break
+#     # ask color to the player
+#     color = ask_color()
 
-        end = ia_turn(WHITE, board)
-        # if end_game(end, draw, WHITE):
-        #     break
-        if end:
-            print("Whites win!")
+# # game main loop
+#     if is_player_first(color):
+#         while True:
+#             e_m, c, end = human_turn(WHITE, board)
+#             if game_over(e_m, WHITE, end):
+#                 break
+
+#             e_m, c, end = ia_turn(BLACK, board)
+#             if game_over(e_m, c, end):
+#                 break
+#     else:
+#         while True:
+#             e_m, c, end = ia_turn(WHITE, board)
+#             if game_over(e_m, c, end):
+#                 break
+
+#             e_m, c, end = human_turn(BLACK, board)
+#             if game_over(e_m, BLACK, end):
+#                 break
+
+    # test loop
+    # TO REMOVE
+    while True:
+        e_m, c, end = ia_turn(WHITE, board)
+        if game_over(e_m, c, end):
             break
-        # time.sleep(2)
-        end = ia_turn(BLACK, board)
-        # if end_game(end, draw, BLACK):
-        #     break
-        if end:
-            print("Blacks win!")
+
+        e_m, c, end = ia_turn(BLACK, board)
+        if game_over(e_m, c, end):
             break
-        # time.sleep(2)
+
 
 if __name__ == "__main__":
     main()
