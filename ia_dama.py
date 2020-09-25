9a92456df09cd97d7b5fb053a891999f6456fc6e
96hoshi <m.locascio96@hotmail.com>
2020-09-25 19:54:59 +0200

    Implemented end_game funcion to better recognize a final evaluation node, removed prints

--- ia_dama.py
+++ ia_dama.py
@@ -1,47 +1,87 @@
 #!/usr/bin/env python3
 
 import math
+import time
 from moves import *
 
-MAX_DEPTH = 3
-min_val = math.inf
-max_val = -math.inf
+MAX_DEPTH = 4
 
-def opposite_color(color):
-    if color == WHITE:
-        return BLACK
-    else:
-        return WHITE
-
-def board_legal_moves(color, board):
-    all_legal_m = []
-
-    for i in range(SIZE):
-        for j in range(SIZE):
-            if color_check(i, j, color, board):
-                l_m = legal_moves(i, j, board)
-                for move in l_m:
-                    all_legal_m.append([(i, j), move])
-
-    return all_legal_m
-
-def perform_move(m, forced, board):
+def perform_move(m, board):
     move = deepcopy(m)
+    eat_move = False
 
     while len(move) > 1:
         i, j = move[0]
         end_i, end_j = move[1]
 
-        movement(i, j, end_i, end_j, forced, board)
+        eat_move = movement(i, j, end_i, end_j, board)
         del move[:1]
 
     if move:
         end_i, end_j = move.pop()
         check_new_dama(end_i, end_j, board)
 
+    return eat_move
+
+def end_game(board):
+    return not (move_aviable(WHITE, board) and move_aviable(BLACK, board))
+
+# # check if there's only one color in the board
+# # and if the enemy cannot do any movement
+def move_aviable(color, board):
+    dama_col = DAMAB
+    if color == WHITE:
+        dama_col = DAMAW
+
+    enemy_color = opposite_color(color)
+
+    for i in range(SIZE):
+        for j in range(SIZE):
+            if board[i][j] == EMPTY:
+                continue
+            if color_check(i, j, color, board):
+                continue
+            else:
+                # if the enemy can do a movement then it's not a win condition
+                enemy_legal = legal_moves(i, j, board)
+                if enemy_legal:
+                    return True
+
+                # if the enemy can eat then it's not a win condition
+                dir_i = 1
+                if enemy_color == WHITE:
+                    dir_i = -1
+                if can_eat(i, dir_i, j, -1, enemy_color, board):
+                    return True
+                if can_eat(i, dir_i, j, 1, enemy_color, board):
+                    return True
+
+                if is_dama(board[i][j]):
+                    dir_i *= -1
+                    if can_eat(i, dir_i, j, -1, enemy_color, board):
+                        return True
+                    if can_eat(i, dir_i, j, 1, enemy_color, board):
+                        return True
+
+    return False
+
+# check if is possible to eat AT LEAST a pedina from a position (i, j)
+def can_eat(i, dir_i, j, dir_j, color, board):
+    if box_legal(i + dir_i, j + dir_j) and board[i + dir_i][j + dir_j] != EMPTY:
+        if not color_check(i + dir_i, j + dir_j, color, board):
+            if box_legal(i + dir_i*2, j + dir_j*2) and board[i + dir_i*2][j + dir_j*2] == EMPTY:
+
+                if not is_dama(board[i + dir_i][j + dir_j]):
+                    # I can eat
+                    return True
+                elif is_dama(board[i][j]):
+                    # I can eat a dama if I'm a dama
+                    return True
+    return False
+
 def eval(board, max_color):
     w1 = 1
-    w2 = 2
+    w2 = 3
 
     whites = 0
     blacks = 0
@@ -63,13 +103,12 @@ def eval(board, max_color):
 
     if max_color == WHITE:
         return -score
-
     return score
 
 def minmax(node, depth, maximizing_player, max_color, min_color):
     global min_val, max_val
 
-    if depth == MAX_DEPTH or win_condition(node):
+    if depth == MAX_DEPTH or end_game(node):
         return eval(node, max_color), []
 
     if maximizing_player:
@@ -77,36 +116,23 @@ def minmax(node, depth, maximizing_player, max_color, min_color):
         max_current = []
         forced_m = board_forced_moves(max_color, node)
         if forced_m:
-            # print("DIM FORCED: {} {}".format(len(forced_m), forced_m))
             for move in forced_m:
                 child = deepcopy(node)
-                perform_move(move, True, child)
-                v, _ = minmax(child, depth + 1, False, max_color, min_color)
-                # print("val: {} moves:{}".format(v, m))
-                if value <= v:
-                    value = v
-                    max_current = move
-                # print("frc-val: {}".format(v), end = "")
-                # print_movement(move, max_color)
-        else:
-            all_legal_m = board_legal_moves(max_color, node)
-            # print("DIM LEGAL: {} {}".format(len(all_legal_m), all_legal_m))
-            for move in all_legal_m:
-                child = deepcopy(node)
-                perform_move(move, False, child)
+                perform_move(move, child)
                 v, _ = minmax(child, depth + 1, False, max_color, min_color)
                 if value < v:
                     value = v
                     max_current = move
-                # print("lgl-val: {}".format(v), end = "")
-                # print_movement(move, max_color)
+        else:
+            all_legal_m = board_legal_moves(max_color, node)
+            for move in all_legal_m:
+                child = deepcopy(node)
+                perform_move(move, child)
+                v, _ = minmax(child, depth + 1, False, max_color, min_color)
+                if value < v:
+                    value = v
+                    max_current = move
 
-
-        max_val = max(max_val, value)
-        min_val = min(min_val, value)
-        # print(" Max: {} move: {}".format(value, m))
-        # print("MAX_val: {}".format(value), end = "")
-        # print_movement(max_current, max_color)
         return value, max_current
 
     else: # minimizing player
@@ -116,52 +142,38 @@ def minmax(node, depth, maximizing_player, max_color, min_color):
         if forced_m:
             for move in forced_m:
                 child = deepcopy(node)
-                perform_move(move, True, child)
-                v, m = minmax(child, depth + 1, True, max_color, min_color)
+                perform_move(move, child)
+                v, _ = minmax(child, depth + 1, True, max_color, min_color)
                 if value > v:
                     value = v
                     min_current = move
-                # print("val-F: {}".format(v), end = "")
-                # print_movement(move, min_color)
         else:
             all_legal_m = board_legal_moves(min_color, node)
             for move in all_legal_m:
                 child = deepcopy(node)
-                perform_move(move, False, child)
-                v, m = minmax(child, depth + 1, True, max_color, min_color)
+                perform_move(move, child)
+                v, _ = minmax(child, depth + 1, True, max_color, min_color)
                 if value > v:
                     value = v
                     min_current = move
-        #         print("val-L: {}".format(v), end = "")
-        #         print_movement(move, min_color)
 
-        # print("MIN_val: {}".format(value), end = "")
-        # print_movement(m, min_color)
         return value, min_current
 
 def ia_turn(player_color, board):
-    print("{} TURN:".format(player_color))
-    global min_val, max_val
-
-    all_forced_m = board_forced_moves(player_color, board)
-
-    move = []
-    forced = False
-
-    if not check_if_empty(all_forced_m):
-        forced = True
+    print("{} turn:".format(player_color))
 
     enemy_color = opposite_color(player_color)
     val, move = minmax(board, 0, True, player_color, enemy_color)
-    print("    VAL: {}".format(val), end = "")
+    # time.sleep(1)
 
-    print("#### MIN: {} MAX: {}".format(min_val, max_val))
-    min_val = math.inf
-    max_val = -math.inf
+    if not move:
+        return False, enemy_color, True
+
+    print("    VAL: {} ".format(val), end = "")
     print("    {} IA move:".format(player_color), end = "")
     print_movement(move, player_color)
 
-    perform_move(move, forced, board)
+    eat_move = perform_move(move, board)
     print_board(board)
 
-    return win_condition(board)
+    return eat_move, player_color, win_condition(player_color, board)
