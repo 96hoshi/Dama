b9d197f695c4585c95b94fe4b776887a05a627df
96hoshi <m.locascio96@hotmail.com>
2020-10-10 16:56:38 +0200

    Refactored evaluation funcion and added more factors. Implemented zero_minmax for tests

--- ia_dama.py
+++ ia_dama.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
 import math
-import time
+import random
 from moves import *
 
-MAX_DEPTH = 4
 
 def perform_move(m, board):
     move = deepcopy(m)
@@ -23,92 +22,194 @@ def perform_move(m, board):
 
     return eat_move
 
+
 def end_game(board):
-    return not (move_aviable(WHITE, board) and move_aviable(BLACK, board))
+    return win_condition(WHITE, board) or win_condition(BLACK, board)
 
-# # check if there's only one color in the board
-# # and if the enemy cannot do any movement
-def move_aviable(color, board):
-    dama_col = DAMAB
+
+def count_pawns(i, j, color):
+    if color == BLACK:
+        return 1
+    elif color == WHITE:
+        return -1
+    return 0
+
+def count_damas(i, j, color):
+    if color == DAMAB:
+        return 1
+    elif color == DAMAW:
+        return -1
+    return 0
+
+def is_pawn_safe(i, j, color):
+    if color == DAMAB or color == DAMAW:
+        return 0
+
+    if (j == 0 or j == 7) or (i == 0 or i == 7):
+        if color == BLACK:
+            return 1
+        if color == WHITE:
+            return -1
+    return 0
+
+def is_dama_safe(i, j, color):
+    if (j == 0 or j == 7) or (i == 0 or i == 7):
+        if color == DAMAB:
+            return 1
+        if color == DAMAW:
+            return -1
+    return 0
+
+def promotion_distance(i, color):
+    if color == BLACK:
+        return i
     if color == WHITE:
-        dama_col = DAMAW
+        return i - (SIZE - 1)
+    return 0
 
-    enemy_color = opposite_color(color)
+# 60.7543 secs con altezza 5
+if False:
+    def eval(board, max_color):
+        w1 = 1
+        w2 = 3
+        w3 = 1
+        w4 = 1
+        w5 = 1
 
-    for i in range(SIZE):
-        for j in range(SIZE):
-            if board[i][j] == EMPTY:
-                continue
-            if color_check(i, j, color, board):
-                continue
-            else:
-                # if the enemy can do a movement then it's not a win condition
-                enemy_legal = legal_moves(i, j, board)
-                if enemy_legal:
-                    return True
+        score = 0
+        for i in range(SIZE):
+            for j in range(SIZE):
+                if board[i][j] is not EMPTY:
+                    score += w1 * count_pawns(i, j, board[i][j])
+                    score += w2 * count_damas(i, j, board[i][j])
+                    score += w3 * is_pawn_safe(i, j, board[i][j])
+                    score += w4 * is_dama_safe(i, j, board[i][j])
+                    score += w5 * promotion_distance(i, board[i][j])
 
-                # if the enemy can eat then it's not a win condition
-                dir_i = 1
-                if enemy_color == WHITE:
-                    dir_i = -1
-                if can_eat(i, dir_i, j, -1, enemy_color, board):
-                    return True
-                if can_eat(i, dir_i, j, 1, enemy_color, board):
-                    return True
+        if max_color == WHITE:
+            return -score
+        return score
 
-                if is_dama(board[i][j]):
-                    dir_i *= -1
-                    if can_eat(i, dir_i, j, -1, enemy_color, board):
-                        return True
-                    if can_eat(i, dir_i, j, 1, enemy_color, board):
-                        return True
+# 55.0345 secs con altezza 5
+else:
+    def eval(board, max_color):
+        w1 = 1
+        w2 = 3
+        w3 = 1
+        w4 = 1
+        w5 = 1
 
-    return False
+        whites = 0
+        blacks = 0
+        white_damas = 0
+        black_damas = 0
+        safe_whites = 0
+        safe_blacks = 0
+        safe_white_damas = 0
+        safe_black_damas = 0
+        white_promotion_dis = 0
+        black_promotion_dis = 0
 
-# check if is possible to eat AT LEAST a pedina from a position (i, j)
-def can_eat(i, dir_i, j, dir_j, color, board):
-    if box_legal(i + dir_i, j + dir_j) and board[i + dir_i][j + dir_j] != EMPTY:
-        if not color_check(i + dir_i, j + dir_j, color, board):
-            if box_legal(i + dir_i*2, j + dir_j*2) and board[i + dir_i*2][j + dir_j*2] == EMPTY:
+        for i in range(SIZE):
+            for j in range(SIZE):
+                if board[i][j] is not EMPTY:
+                    if board[i][j] == WHITE:
+                        whites += 1
+                        safe_whites += is_safe(i, j)
+                        white_promotion_dis += (SIZE - 1) - i
 
-                if not is_dama(board[i + dir_i][j + dir_j]):
-                    # I can eat
-                    return True
-                elif is_dama(board[i][j]):
-                    # I can eat a dama if I'm a dama
-                    return True
-    return False
+                    elif board[i][j] == DAMAW:
+                        white_damas += 1
+                        safe_white_damas += is_safe(i, j)
 
-def eval(board, max_color):
-    w1 = 1
-    w2 = 3
+                    elif board[i][j] == BLACK:
+                        blacks += 1
+                        safe_blacks += is_safe(i, j)
+                        black_promotion_dis += i
 
-    whites = 0
-    blacks = 0
-    white_damas = 0
-    black_damas = 0
+                    elif board[i][j] == DAMAB:
+                        black_damas += 1
+                        safe_black_damas += is_safe(i, j)
 
-    for i in range(SIZE):
-        for j in range(SIZE):
-            if board[i][j] == WHITE:
-                whites += 1
-            if board[i][j] == DAMAW:
-                white_damas += 1
-            if board[i][j] == BLACK:
-                blacks += 1
-            if board[i][j] == DAMAB:
-                black_damas += 1
+        pawns = blacks - whites
+        damas = black_damas - white_damas
+        safe_paws = safe_blacks - safe_whites
+        safe_damas = safe_black_damas - safe_white_damas
+        promotion_distance = black_promotion_dis - white_promotion_dis
 
-    score = w1*(blacks - whites) + w2*(black_damas - white_damas)
+        score = w1 * pawns + w2 * damas + w3 * safe_paws + w4 * safe_damas + w5 * promotion_distance
 
-    if max_color == WHITE:
-        return -score
-    return score
+        if max_color == WHITE:
+            return -score
+        return score
+
+
+def is_safe(i, j):
+    if (j == 0 or j == 7) or (i == 0 or i == 7):
+        return 1
+
+    return 0
+
+
+# def minmax(node, depth, maximizing_player, max_color, min_color):
+#     global min_val, max_val
+
+#     if depth == 0 or end_game(node):
+#         return eval(node, max_color), []
+
+#     if maximizing_player:
+#         value = -math.inf
+#         max_current = []
+#         forced_m = board_forced_moves(max_color, node)
+#         if forced_m:
+#             for move in forced_m:
+#                 child = deepcopy(node)
+#                 perform_move(move, child)
+#                 v, _ = minmax(child, depth - 1, False, max_color, min_color)
+#                 if value < v:
+#                     value = v
+#                     max_current = move
+#         else:
+#             all_legal_m = board_legal_moves(max_color, node)
+#             for move in all_legal_m:
+#                 child = deepcopy(node)
+#                 perform_move(move, child)
+#                 v, _ = minmax(child, depth - 1, False, max_color, min_color)
+#                 if value < v:
+#                     value = v
+#                     max_current = move
+
+#         return value, max_current
+
+#     else:  # minimizing player
+#         value = math.inf
+#         min_current = []
+#         forced_m = board_forced_moves(min_color, node)
+#         if forced_m:
+#             for move in forced_m:
+#                 child = deepcopy(node)
+#                 perform_move(move, child)
+#                 v, _ = minmax(child, depth - 1, True, max_color, min_color)
+#                 if value > v:
+#                     value = v
+#                     min_current = move
+#         else:
+#             all_legal_m = board_legal_moves(min_color, node)
+#             for move in all_legal_m:
+#                 child = deepcopy(node)
+#                 perform_move(move, child)
+#                 v, _ = minmax(child, depth - 1, True, max_color, min_color)
+#                 if value > v:
+#                     value = v
+#                     min_current = move
+
+#         return value, min_current
+
 
 def minmax(node, depth, maximizing_player, max_color, min_color):
     global min_val, max_val
 
-    if depth == MAX_DEPTH or end_game(node):
+    if depth == 0 or end_game(node):
         return eval(node, max_color), []
 
     if maximizing_player:
@@ -119,7 +220,7 @@ def minmax(node, depth, maximizing_player, max_color, min_color):
             for move in forced_m:
                 child = deepcopy(node)
                 perform_move(move, child)
-                v, _ = minmax(child, depth + 1, False, max_color, min_color)
+                v, _ = minmax(child, depth - 1, False, max_color, min_color)
                 if value < v:
                     value = v
                     max_current = move
@@ -128,14 +229,14 @@ def minmax(node, depth, maximizing_player, max_color, min_color):
             for move in all_legal_m:
                 child = deepcopy(node)
                 perform_move(move, child)
-                v, _ = minmax(child, depth + 1, False, max_color, min_color)
+                v, _ = minmax(child, depth - 1, False, max_color, min_color)
                 if value < v:
                     value = v
                     max_current = move
 
         return value, max_current
 
-    else: # minimizing player
+    else:  # minimizing player
         value = math.inf
         min_current = []
         forced_m = board_forced_moves(min_color, node)
@@ -143,7 +244,7 @@ def minmax(node, depth, maximizing_player, max_color, min_color):
             for move in forced_m:
                 child = deepcopy(node)
                 perform_move(move, child)
-                v, _ = minmax(child, depth + 1, True, max_color, min_color)
+                v, _ = minmax(child, depth - 1, True, max_color, min_color)
                 if value > v:
                     value = v
                     min_current = move
@@ -152,25 +253,43 @@ def minmax(node, depth, maximizing_player, max_color, min_color):
             for move in all_legal_m:
                 child = deepcopy(node)
                 perform_move(move, child)
-                v, _ = minmax(child, depth + 1, True, max_color, min_color)
+                v, _ = minmax(child, depth - 1, True, max_color, min_color)
                 if value > v:
                     value = v
                     min_current = move
 
         return value, min_current
 
-def ia_turn(player_color, board):
+
+def zero_minmax(color, board):
+    forced_m = board_forced_moves(color, board)
+    if forced_m:
+        return 0, random.choice(forced_m)
+    else:
+        legal_m = board_legal_moves(color, board)
+        if legal_m:
+            return 0, random.choice(legal_m)
+
+    return 0, []
+
+
+def ia_turn(player_color, max_depth, board):
     print("{} turn:".format(player_color))
 
     enemy_color = opposite_color(player_color)
-    val, move = minmax(board, 0, True, player_color, enemy_color)
+
+    if max_depth == 0:
+        val, move = zero_minmax(player_color, board)
+    else:
+        val, move = minmax(board, max_depth, True, player_color, enemy_color)
     # time.sleep(1)
+
+    print("    VAL: {} ".format(val), end="")
+    print("    {} IA move:".format(player_color), end="")
 
     if not move:
         return False, enemy_color, True
 
-    print("    VAL: {} ".format(val), end = "")
-    print("    {} IA move:".format(player_color), end = "")
     print_movement(move, player_color)
 
     eat_move = perform_move(move, board)
