e6bc7f54e1f3c9f5f76e1a5619b0a88769d3a198
96hoshi <m.locascio96@hotmail.com>
2020-10-21 16:50:30 +0200

    Now launch five games at time

--- dama_script.sh
+++ dama_script.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-# #same depth games
-# for i in `seq 0 5`; do
-# 	for k in `seq 1 10`; do
-# 		python3 game.py --white_depth ${i} --black_depth ${i}
-# 	done
-# done
+# permutation of all games simulations
+for i in `seq 0 4`; do
+	for j in `seq 0 4`; do
+		# doing 40 games
+		for k in `seq 0 7`; do
 
-# 
-for i in `seq 0 6`; do
-	for j in `seq 0 6`; do
-		for k in `seq 1 10`; do
-			python3 game.py --white_depth ${i} --black_depth ${j} 
+			# launch 5 games at time
+			for p in `seq 0 4`; do
+				python3 game.py --white_depth ${i} --black_depth ${j} > /dev/null &
+				pids[${p}]=$!
+			done
+
+			# wait for all pids
+			for pid in ${pids[*]}; do
+				wait $pid
+			done
 		done
 	done
 done
 
-#
-for i in `seq 0 6`; do
-	for j in `seq 0 6`; do
-		for k in `seq 1 10`; do
-			python3 game.py --white_depth ${j} --black_depth ${i}
-		done
-	done
-done
-
+python3 time_test.py
